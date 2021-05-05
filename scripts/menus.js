d9a50d2214d97b81f55fa893deb9f7ab0bdc7527
Lucas Regis de Moraes <LucasGRM03@gmail.com>
2021-05-05 09:34:04 -0300

    Avançado o desenvolvimento do desafio

---
+++ scripts/menus.js
@@ -0,0 +1,35 @@
+function openMenu() {
+    const menu = document.getElementById('menu')
+    menu.classList.replace('menuClosed', 'menuOpened')
+    const hamburguer = document.getElementById('hamburguerMenu')
+    hamburguer.setAttribute('src', './images/icon-close.svg')
+    hamburguer.setAttribute('onclick', 'closeMenu()')
+}
+
+function closeMenu() {
+    const menu = document.getElementById('menu')
+    menu.classList.replace('menuOpened', 'menuClosed')
+    const hamburguer = document.getElementById('hamburguerMenu')
+    hamburguer.setAttribute('src', './images/icon-hamburger.svg')
+    hamburguer.setAttribute('onclick', 'openMenu()')
+}
+
+function openProductMenu() {
+    const options = document.getElementById('productOptions')
+    options.classList.replace('menuClosed', 'menuOpened')
+
+    const option = document.getElementById('productOption')
+    option.setAttribute('onclick', 'closeProductMenu()')
+
+    console.log(option)
+}
+
+function closeProductMenu() {
+    const options = document.getElementById('productOptions')
+    options.classList.replace('menuOpened', 'menuClosed')
+
+    const option = document.getElementById('productOption')
+    option.setAttribute('onclick', 'openProductMenu()')
+
+    console.log(option)
+}
